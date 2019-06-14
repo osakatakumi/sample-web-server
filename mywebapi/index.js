7b4763f8c59f0740f2d51df731c0323cfff484d1
osakatakumi <osuka0309@gmail.com>
2019-06-14 17:50:13 +0900

    6/14 課題

---
+++ mywebapi/index.js
@@ -0,0 +1,24 @@
+// expressモジュールを読み込む
+const express = require('express');
+
+// expressアプリを生成する
+const app = express();
+
+// webフォルダの中身を公開する
+app.use(express.static('web'));
+
+//　htttp://localhost:3000/api/v1/listにアクセスしたときに
+// TODOリストを返す
+app.get('/api/v1/list', (req, res) => {
+    //　クライアントに送るJSONデータ
+    const todoList = [
+        { title: 'JavaScriptを勉強する', done: true},
+        { tiele: 'Node.jsを勉強する', done: false},
+        { title: 'Web APIを作る', done: false}
+    ];
+    // JSONを送信する
+    res.json(todoList);
+});
+//　ポート3000でサーバーを立てる
+app.listen(3000, () => console.log('Listening on port 3000'));
+
